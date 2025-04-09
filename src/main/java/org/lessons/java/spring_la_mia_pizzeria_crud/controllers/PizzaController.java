a324150d7c0b0b1f66f27cc4fc8f2905efc3aac3
MassimilianoTagliabue <massimilianotagliabue.04@gmail.com>
2025-04-09 21:33:03 +0200

    feat: form pizza

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
@@ -7,10 +7,18 @@ import org.lessons.java.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @Controller
@@ -43,5 +51,16 @@ public class PizzaController {
         return "pizzas/index";
     }
 
+    @GetMapping("/pizza/create")
+    public String create(Model model){
+        model.addAttribute("pizza", new Pizza());
+        return "pizzas/create";
+    }
+
+    @PostMapping("/pizza/create")
+    public String store (@Valid @ModelAttribute ("pizza") Pizza pizzaForm, BindingResult bindingResult, Model model) {
+
+        return "pizzas/create";
+    }
     
 }
